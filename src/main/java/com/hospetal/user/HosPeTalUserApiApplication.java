5346c6712a01fbc5d4180b3f5ca08b22de1ba305
River Kim <bluuminn@gmail.com>
2023-01-04 22:32:22 +0900

    Change package name

--- src/main/java/com/hospetal/user/HosPeTalUserApiApplication.java
+++ src/main/java/com/hospetal/user/HosPeTalUserApiApplication.java
@@ -1,4 +1,4 @@
-package com.hospetal.petowner;
+package com.hospetal.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
