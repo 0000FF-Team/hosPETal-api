6809ab93d8e817856e953fc32a0cccff42afc248
River Kim <bluuminn@gmail.com>
2023-01-12 22:33:09 +0900

    feat: 병원 상세 조회 구현

--- src/main/java/com/hospetal/user/service/HospitalService.java
+++ src/main/java/com/hospetal/user/service/HospitalService.java
@@ -2,11 +2,13 @@ package com.hospetal.user.service;
 
 import com.hospetal.user.domain.entity.Hospital;
 import com.hospetal.user.domain.repository.HospitalRepository;
+import com.hospetal.user.dto.response.HospitalDetailDto;
 import com.hospetal.user.dto.response.HospitalListDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,11 +22,27 @@ public class HospitalService {
 
     public List<HospitalListDto> getHospitals() {
         List<Hospital> hospitals = hospitalRepository.findAll();
-        return hospitals.stream().map(HospitalService::toDto).collect(Collectors.toList());
+        return hospitals.stream().map(this::toListDto).collect(Collectors.toList());
     }
 
-    private static HospitalListDto toDto(Hospital hospital) {
+    private HospitalListDto toListDto(Hospital hospital) {
         return HospitalListDto.builder()
+                .id(hospital.getId())
+                .name(hospital.getName())
+                .contactNumber(hospital.getContactNumber())
+                .description(hospital.getDescription())
+                .latitude(hospital.getLatitude())
+                .longitude(hospital.getLongitude())
+                .build();
+    }
+
+    public HospitalDetailDto getHospital(Long id) {
+        Hospital hospital = hospitalRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        return toDetailDto(hospital);
+    }
+
+    private HospitalDetailDto toDetailDto(Hospital hospital) {
+        return HospitalDetailDto.builder()
                 .id(hospital.getId())
                 .name(hospital.getName())
                 .contactNumber(hospital.getContactNumber())
